6f4d54c45e944e6da851b11cadbe8519247083ae
Piotr Preciuk <piotr.preciukbusiness@gmail.com>
2023-09-23 19:07:11 +0200

    Adding localstorage

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+
   useEffect(() => {
+    if (tasks.length === 0) return;
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    setTasks(tasks);
+  }, []);
+
   function addTask(name) {
     setTasks((previous) => {
       return [...previous, { name: name, done: false }];
